f6e0b673c58907ec9ae21930424b0539979f49da
Kristiyan <kristiangk@abv.bg>
2020-04-23 18:18:39 +0300

    Added EasyMVVM project which shows an example MVVM implementation for WPF apps

---
+++ EasyMVVM/MainWindowVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+
+namespace EasyMVVM
+{
+    public class MainWindowVM : DependencyObject, INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _backingProperty;
+
+        public MainWindowVM()
+        {
+            Model m = new Model();
+
+            BoundProperty = m.GetData();
+        }
+
+        public ObservableCollection<string> BoundProperty
+        {
+            get => _backingProperty;
+            set
+            {
+                _backingProperty = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ EasyMVVM/Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace EasyMVVM
+{
+    public class Model
+    {
+        public Model()
+        {
+            _data = new ObservableCollection<string>(new[]
+            {
+                "First Entry", "Second Entry", "Third Entry"
+            });
+        }
+
+        private ObservableCollection<string> _data;
+
+        public ObservableCollection<string> GetData() => _data;
+    }
+}
